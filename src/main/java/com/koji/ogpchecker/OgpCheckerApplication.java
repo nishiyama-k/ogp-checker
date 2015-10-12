d73df0612dc444a3de3e654ce037f53e51cfbaa3
nishiyama-k <nishi.comcom@gmail.com>
2015-10-12 23:12:07 +0900

    i18n

--- src/main/java/com/koji/ogpchecker/OgpCheckerApplication.java
+++ src/main/java/com/koji/ogpchecker/OgpCheckerApplication.java
@@ -1,12 +1,45 @@
 package com.koji.ogpchecker;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
-public class OgpCheckerApplication {
+public class OgpCheckerApplication extends WebMvcConfigurerAdapter {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OgpCheckerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(OgpCheckerApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+		slr.setDefaultLocale(Locale.US);
+		return slr;
+	}
+
+	/**
+	 * query parameter "lang" determines language
+	 * 
+	 * @return
+	 */
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+		lci.setParamName("lang");
+		return lci;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+
 }

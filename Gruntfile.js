04d2ccda38db76f290c971cd922fba2f04937557
nishiyama-k <nishi.comcom@gmail.com>
2015-10-25 16:59:23 +0900

    fix css

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,12 +12,22 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-clean');
 
     grunt.initConfig({
+        // js check
+        jshint: {
+            all: ['Gruntfile.js', path + '/js/*.js']
+        },
+        // javascript phase
+        jsbeautifier: {
+            files: ['Gruntfile.js', path + "/js/*.js",
+                path + "/template/*.html"
+            ]
+        },
         concat: {
             jsapp: {
                 src: [path + '/js/lib/jquery-1.11.3.js',
                     path + '/js/lib/jquery-ui.js',
-                    path + '/js/lib/jquery.autocomplete.js',
                     path + '/js/lib/jquery.bootstrap.js',
+                    path + '/js/lib/jquery.autocomplete.js',
                     path + '/js/app.js'
                 ],
                 dest: path + '/js/build/application.js'
@@ -29,22 +39,11 @@ module.exports = function(grunt) {
                 dest: path + '/js/build/application.min.js'
             }
         },
-        cssmin: {
-            cssapp: {
-                src: [path + '/css/lib/bootstrap.css',
-                    path + '/css/lib/bootswatch.css',
-                    path + '/css/lib/jquery-ui.min.css',
-                    path + '/css/lib/jquery-ui.theme.min.css',
-                    path + '/css/plugin/jquery.autocomplete.css',
-                    path + '/css/app.css'
-                ],
-                dest: path + '/css/build/application.min.css'
-            }
-        },
+        // css phase
         less: {
             bootstrap: {
                 src: 'src/main/less/bootstrap/bootstrap.less',
-                dest: path + '/css/lib/bootstrap.css'
+                dest: path + '/css/build/bootstrap.css'
             },
             // theme: {
             // src: 'src/main/less/bootstrap/theme.less',
@@ -52,32 +51,45 @@ module.exports = function(grunt) {
             // },
             paper: {
                 src: 'src/main/less/bootstrap/paper/bootswatch.less',
-                dest: path + '/css/lib/bootswatch.css'
+                dest: path + '/css/build/bootswatch.css'
+            },
+            awesome: {
+                src: 'src/main/less/font-awesome-4.4.0/font-awesome.less',
+                dest: path + '/css/build/font-awesome.css'
             },
             app: {
                 src: 'src/main/less/app.less',
                 dest: path + '/css/app.css'
             }
         },
+        cssmin: {
+            cssapp: {
+                src: [path + '/css/lib/jquery-ui.min.css',
+                    path + '/css/lib/jquery-ui.theme.min.css',
+                    path + '/css/build/font-awesome.css',
+                    path + '/css/build/bootstrap.css',
+                    path + '/css/build/bootswatch.css',
+                    path + '/css/lib/jquery.autocomplete.css',
+                    path + '/css/app.css'
+                ],
+                dest: path + '/css/build/application.min.css'
+            }
+        },
+        // additional
         watch: {
             less: {
                 files: ['src/main/less/*.less'],
-                tasks: ['less']
+                tasks: ['less:app']
             }
         },
-        jshint: {
-            all: ['Gruntfile.js', path + '/js/*.js']
-        },
-        jsbeautifier: {
-            files: ['Gruntfile.js', path + "/js/*.js", path + "/template/*.html"]
-        },
         clean: {
-            js: ['<%= concat.jsapp.dest %>']
+            js: ['src/main/resources/static/js/build'],
+            css: ['src/main/resources/static/css/build']
         }
     });
 
-    grunt.registerTask('default', ['less', 'concat', 'uglify', 'cssmin',
-        'jshint', 'jsbeautifier', 'clean'
+    grunt.registerTask('default', ['jshint', 'jsbeautifier', 'concat',
+        'uglify', 'less', 'cssmin'
     ]);
     grunt.registerTask('lessmin', ['less', 'cssmin']);
     grunt.registerTask('jsmin', ['concat', 'uglify']);
---
+++ README.md
@@ -0,0 +1,6 @@
+# This is my sample project
+- This README.md is my memo.
+
+
+# how to deploy
+``mvn clean heroku:deploy``
